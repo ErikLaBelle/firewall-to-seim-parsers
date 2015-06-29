2bca37ec9e0be28a6e1de0d535ffc6154d113b29
Erik LaBelle <e_labelle@sbcglobal.net>
2015-06-29 12:19:49 -0500

    sql script to insert plugin
    This sql script delete any value assigned to the plugin library for the chosen ID and subcatergory ID.
    
    Next it insert new values for id,type,name,Product_type,vendor,and description
    Finally it insert new value for subcatergory for plugin_id,sid,category_id,class_id,name,priority,and reliability

---
+++ barrier1.sql
@@ -0,0 +1,10 @@
+DELETE FROM plugin where id="9002";
+DELETE FROM plugin_sid where plugin_id ="9002";
+Insert into plugin (id,type,name,Product_type,vendor,description) VALUES (9002,1,'Barrier1',10,'Barrier1','remote syslog from Barrier1 firewall');
+Insert into plugin_sid (plugin_id,sid,category_id,class_id,name,priority,reliability) VALUES (9002,1,NULL,NULL,'Firewall Event with DF',5,5);
+Insert into plugin_sid (plugin_id,sid,category_id,class_id,name,priority,reliability) VALUES (9002,2,NULL,NULL,'Firewall Event with No DF',5,5);
+Insert into plugin_sid (plugin_id,sid,category_id,class_id,name,priority,reliability) VALUES (9002,3,NULL,NULL,'Firewall Event with IMCP and No DF',5,5);
+Insert into plugin_sid (plugin_id,sid,category_id,class_id,name,priority,reliability) VALUES (9002,4,NULL,NULL,'Firewall Event with IMCP and DF',5,5);
+Insert into plugin_sid (plugin_id,sid,category_id,class_id,name,priority,reliability) VALUES (9002,10,NULL,NULL,'DHCP Inform',5,5);
+Insert into plugin_sid (plugin_id,sid,category_id,class_id,name,priority,reliability) VALUES (9002,11,NULL,NULL,'Promiscious Mode',5,5);
+Insert into plugin_sid (plugin_id,sid,category_id,class_id,name,priority,reliability) VALUES (9002,99,NULL,NULL,'Catch all Event',5,5);
