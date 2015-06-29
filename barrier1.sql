ada9dec1f4c0fd59ee7ef63388bbdf46152bf248
Erik LaBelle <e_labelle@sbcglobal.net>
2015-06-29 18:09:03 -0500

    Update barrier1.sql

--- barrier1.sql
+++ barrier1.sql
@@ -1,3 +1,8 @@
+# Script to delete existing plugin ID and create and insert new plugin ID.
+# Built following instructions in "How to create a data source plugin" by Natividad Baena from AlienVault.
+# See Baena's instructions on how to load the .cfg config file and execute the .sql SQL file:
+# https://alienvault.bloomfire.com/posts/747999-how-to-create-a-data-source-plugin
+
 DELETE FROM plugin where id="9002";
 DELETE FROM plugin_sid where plugin_id ="9002";
 Insert into plugin (id,type,name,Product_type,vendor,description) VALUES (9002,1,'Barrier1',10,'Barrier1','remote syslog from Barrier1 firewall');
